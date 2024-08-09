05c4d4853c8400b04d6111889d996cded304a4bc
sameer-gits <ms3054825@gmail.com>
2024-08-10 00:55:30 +0530

    added mask anition

--- slider/scripts.js
+++ slider/scripts.js
@@ -1,3 +1,11 @@
 function slide() {
   console.log("Slide it!");
 }
+
+document.getElementById("can-1").addEventListener("pointerover", function () {
+  document.getElementById("label").classList.add("label-hover");
+});
+
+document.getElementById("can-1").addEventListener("pointerout", function () {
+  document.getElementById("label").classList.remove("label-hover");
+});
