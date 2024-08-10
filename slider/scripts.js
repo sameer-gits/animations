2aab39f84b357a85533c2efb0151f3fcbc55ca8a
sameer-gits <ms3054825@gmail.com>
2024-08-10 23:58:49 +0530

    added mouse animation

--- slider/scripts.js
+++ slider/scripts.js
@@ -1,6 +1,5 @@
 function slide() {
   const mask = document.getElementById("mask-linear");
-
   if (mask.style.maskPosition === "0% 0%") {
     mask.style.maskPosition = "100% 100%";
   } else {
@@ -9,3 +8,15 @@ function slide() {
 
   console.log("Slide it!");
 }
+
+const label1 = document.getElementById("label1");
+const label2 = document.getElementById("label2");
+
+document.addEventListener("mousemove", (e) => {
+  const x = e.clientX;
+  const y = e.clientY;
+  let translateX = Math.min(Math.max(x / 100, -30), 35);
+  label1.style.transform = `translate(${translateX}%, 0%)`;
+  label2.style.transform = `translate(${translateX}%, 0%)`;
+  console.log(translateX);
+});
